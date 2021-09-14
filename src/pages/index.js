9ffa74171621f9b3b166cd007c90bb830ba9b154
Woodoru <90560945+Woodoru@users.noreply.github.com>
2021-09-14 14:59:11 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <main>
       <Layout pageTitle = 'Home Page'>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        src = "https://cdnb.artstation.com/p/assets/images/images/019/775/347/large/kate-kneebone-su-background-style-sunset.jpg?1564979276"
+        alt = "Any image"
       </Layout>
     </main>
   )

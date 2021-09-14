34a4e235910aaf38e628f311fc63302387ce89cf
Woodoru <90560945+Woodoru@users.noreply.github.com>
2021-09-14 14:26:00 +0900

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 const AboutPage = () => {
   return (
     <main>
-      <title>About Me</title>
+      <Layout pageTitle = 'About me'>
       <h1>About Me</h1>
-      <Link to="/">Back to Home</Link>
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      </Layout>
     </main>
   )
 }

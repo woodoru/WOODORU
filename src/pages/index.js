863f336479f1078d3e82dc0779a65217eedd7808
Woodoru <90560945+Woodoru@users.noreply.github.com>
2021-09-14 14:22:01 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,7 @@ const IndexPage = () => {
   return (
     <main>
       <Layout pateTitle = "Home Page">
-      <title>Home Page</title>
       <h1>Welcome to my Gatsby site!</h1>
-      <Link to = "/about">About</Link>
       <p>I'm making this by following the Gatsby Tutorial.</p>
       </Layout>
     </main>

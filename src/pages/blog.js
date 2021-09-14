6c213bf08bd748894b8e0955e8d1cd1eea418581
Woodoru <90560945+Woodoru@users.noreply.github.com>
2021-09-14 20:24:17 +0900

    Create blog.js

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const BlogPage = () => {
+  return (
+    <Layout pageTitle="Posts">
+      <p>My posts will go in here</p>
+    </Layout>
+  )
+}
+
+export default BlogPage

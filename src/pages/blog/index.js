7338826b60c3e1262237f2e1f6d93102d627b4dd
Woodoru <90560945+Woodoru@users.noreply.github.com>
2021-09-15 15:01:15 +0900

    Update index.js

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../../components/layout'
 
